29070103e5900f5031d899d42fd74b3dd0b99b72
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-06-06 13:55:15 -0400

    Added pipx

--- installs.sh
+++ installs.sh
@@ -1,4 +1,5 @@
-zypper install radare2 nodejs-common
+zypper install radare2 nodejs-common python313-pipx
+
 pip install frida-tools # CLI tools
 pip install frida       # Python bindings
 npm install frida       # Node.js bindings
