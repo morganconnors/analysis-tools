2cb59ee95f5edf50f89325723cda8040b703517f
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-06-06 15:19:10 -0400

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,6 +1,15 @@
-zypper install radare2 nodejs-common python313-pipx
+zypper install radare2 nodejs-common python313-pipx neovim
+wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.2_build/ghidra_11.3.2_PUBLIC_20250415.zip
 
-mkdir ~/Analysis && cd ~/Analysis/
+7z x ghidra_11.3.2_PUBLIC_20250415.zip
+mv ghidra_11.3.2_PUBLIC /usr/local/bin/
+echo "export PATH=\"/usr/local/bin/ghidra_11.3.2_PUBLIC:$PATH\"" >> ~/.bashrc
+
+mkdir ~/Analysis
 
 pipx install frida-tools # CLI tools
 npm install frida       # Node.js bindings
+
+cd ~/Analysis/
+
+echo "/usr/lib64/jvm/jre-21-openjdk/bin/java"
